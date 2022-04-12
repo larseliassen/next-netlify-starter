a20bf6aacae83349b426b1b98c97314470b49861
Lars Moland Eliassen <lars@Larss-MacBook-Air.local>
2022-04-12 23:00:46 +0200

    fix

--- components/Heading.js
+++ components/Heading.js
@@ -1,8 +1,24 @@
-  export default function Heading() {
-    return (
+import React, { useState, useEffect, useCallback } from "react";
+const names = [
+  'BJØRK', 'ROGN', 'SELJE', 'VED'
+]
+
+export default function Heading() {
+  const [newName, setnewName] = useState("");
+
+    const shuffle = useCallback(() => {
+        const index = Math.floor(Math.random() * names.length);
+        setnewName(names[index]);
+    }, []);
+
+    useEffect(() => {
+        const intervalID = setInterval(shuffle, 800);
+        return () => clearInterval(intervalID);
+    }, [shuffle])
+
+  return (
     <h1 className="clipped-heading" >
-        
+      KNUSK<br />TØRR<br /><span>{newName}</span>
     </h1>
-    )
-  }
-  
+  )
+}
